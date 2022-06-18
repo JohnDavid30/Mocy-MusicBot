4dc1dfa160435955cdd48cfc1b22bc3a3978cbab
JohnDavid <76064400+MrrVit665@users.noreply.github.com>
2022-06-18 10:48:04 +0700

    Add files via upload

--- shard.js
+++ shard.js
@@ -1,1 +1,11 @@
+const {ShardingManager} = require('discord.js');
+const config = require('./config.js');
 
+const shards = new ShardingManager("./index.js", {
+    token: config.token,
+    totalShards: "auto"
+});
+shards.on("shardCreate", shard =>{
+    console.log(`[READY] ${new Date().toString().split(" ",5).join(" ")} Launched Shard #${shard.id}`);
+});
+shards.spawn(shards.totalShards, 10000)
