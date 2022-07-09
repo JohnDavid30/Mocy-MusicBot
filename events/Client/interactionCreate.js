7f653990f640628302899727302f6584bd0cb08f
JohnDavid <76064400+MrrVit665@users.noreply.github.com>
2022-07-10 06:50:20 +0700

    Update interactionCreate.js

--- events/Client/interactionCreate.js
+++ events/Client/interactionCreate.js
@@ -4,50 +4,6 @@ module.exports = {
     name: "interactionCreate",
     run: async (client, interaction) => {
       if (interaction.isButton()) {
-            if (interaction.guild.me.voice.channel && !interaction.guild.me.voice.channel.equals(interaction.member.voice.channel)) return;
-        
-            const player = client.manager.get(interaction.guild.id);
-            if (!player) return;
-        let channel = client.channels.cache.get(player.textChannel);
-  if (!channel) return;
-            switch (interaction.customId) {
-                case 'PREVIOUS_BUTTON': {
-                    if (player.queue.previous) {
-                        player.seek(0);
-
-                        const embed = new Discord.MessageEmbed()
-                            .setColor(client.embedColor)
-                            .setAuthor(`Backing up to ${player.queue.current.title}`, interaction.member.displayAvatarURL());
-                        await channel.send({ embeds: [embed] });
-                    }
-                    break;
-                }
-                case 'PAUSE_BUTTON': {
-                    const buttonRow = interaction.message.components[0];
-                    player.pause(!player.paused);
-                    buttonRow.components[1] = new Discord.MessageButton()
-                        .setCustomId('PAUSE_BUTTON')
-                        .setStyle('PRIMARY')
-                        .setEmoji(player.paused ? client.emoji.resume : client.emoji.pause);
-
-                    const embed = new Discord.MessageEmbed()
-                        .setColor(client.embedColor)
-                        .setAuthor(`Song is now ${player.playing ? 'resumed' : 'paused'}.`, interaction.member.displayAvatarURL());
-                    await client.channels.cache.get(player.textChannel).send({ embeds: [embed] });
-                    await interaction.update({ components: [buttonRow] });
-                    break;
-                }
-                case 'SKIP_BUTTON': {
-                    const title = player.queue.current.title;
-                    if (player.trackRepeat) player.setTrackRepeat(false);
-                    if (player) player.stop();
-
-                const embed = new Discord.MessageEmbed()const Discord  = require("discord.js");
-
-module.exports = {
-    name: "interactionCreate",
-    run: async (client, interaction) => {
-      if (interaction.isButton()) {
             if (interaction.guild.me.voice.channel && !interaction.guild.me.voice.channel.equals(interaction.member.voice.channel)) return await interaction.reply({content: `You are not connected to ${interaction.guild.me.voice.channel} to use this buttons.`, ephemeral: true});
 
                 
@@ -223,12 +179,4 @@ module.exports = {
             }
     }
 };
-                        .setColor(client.embedColor)
-                        .setAuthor(`Skipped ${title}`, interaction.member.displayAvatarURL());
-                    await client.channels.cache.get(player.textChannel).send({ embeds: [embed] });
-                    break;
-                }
-            }
-            }
-    }
-};
+
